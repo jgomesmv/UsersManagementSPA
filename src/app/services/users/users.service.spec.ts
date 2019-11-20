4ff540461d4d8bc9c6cdab6f88416799bbc40ff8
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-20 17:03:34 +0000

    add user service tests

--- src/app/services/users/users.service.spec.ts
+++ src/app/services/users/users.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, async } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UsersService;
+  let httpMock: HttpTestingController;
+
+  const testUser = {name: 'testUser', previousName: null};
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+
+    service = TestBed.get(UsersService);
+    httpMock = TestBed.get(HttpTestingController);
+  }));
 
   it('should be created', () => {
-    const service: UsersService = TestBed.get(UsersService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get the list of users', () => {
+    const expectedUrl = 'https://uitest.free.beeceptor.com/usernames';
+    const expectedResponse = [testUser];
+
+    service.get().subscribe(result => {
+      expect(result).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(expectedUrl);
+    req.flush(expectedResponse);
   });
 });
