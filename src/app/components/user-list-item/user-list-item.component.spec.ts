d80a46f2436a0b5ce4715ce80a82252f31cebfbd
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-20 11:10:27 +0000

    implement user list item unit tests

--- src/app/components/user-list-item/user-list-item.component.spec.ts
+++ src/app/components/user-list-item/user-list-item.component.spec.ts
@@ -1,14 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { UserFormComponent } from '../user-form/user-form.component';
 import { UserListItemComponent } from './user-list-item.component';
+import { UsersDataSource } from "src/app/data-sources/users/users.data-source";
+import { of } from "rxjs";
 
 describe('UserListItemComponent', () => {
   let component: UserListItemComponent;
   let fixture: ComponentFixture<UserListItemComponent>;
 
+  const testUser = {name: 'testUser', previousName: null};
+
+  const usersDataSourceMock = {
+    updateUser: (user: any) => {},
+    removeUser: (user: any) => {}
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListItemComponent ]
+      imports: [
+        BrowserModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        UserListItemComponent,
+        UserFormComponent
+      ],
+      providers: [
+        { provide: UsersDataSource, useValue: usersDataSourceMock },
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +38,37 @@ describe('UserListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListItemComponent);
     component = fixture.componentInstance;
+    component.user = testUser;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should pass to edit mode after onEdit', () => {
+    component.onEdit();
+    expect(component.mode).toEqual(component.viewModes.editMode);
+  });
+
+  it('should remove user after onDelete', () => {
+    const removeUserSpy = spyOn(
+      TestBed.get(UsersDataSource),
+      'removeUser'
+    );
+    component.onDelete();
+    expect(removeUserSpy).toHaveBeenCalled();
+  });
+
+  it('should update user after onUserUpdated', () => {
+    const updateUserSpy = spyOn(
+      TestBed.get(UsersDataSource),
+      'updateUser'
+    );
+
+    const updatedUser = {name: 'testUserUpdated', previousName: null};
+    component.onUserUpdated(updatedUser);
+    expect(updateUserSpy).toHaveBeenCalled();
+    expect(updatedUser.previousName).toBe(component.user.name);
   });
 });
