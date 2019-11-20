a2903cb32a76e13d7c3ebf711f6f32498da185e9
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-20 10:17:18 +0000

    implement app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,60 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserFormComponent } from "./components/user-form/user-form.component";
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { UserListItemComponent } from "./components/user-list-item/user-list-item.component";
+import { UsersDataSource } from './data-sources/users/users.data-source';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  const usersDataSourceMock = {
+    connect: () => of([]),
+    disconnect: () => {},
+    addUser: (user: any) => {}
+  };
+
+  let fixture: any;
+  let app: any;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule
+
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        UserListItemComponent,
+        UserFormComponent,
+        UserListComponent
       ],
+      providers: [
+        { provide: UsersDataSource, useValue: usersDataSourceMock },
+      ]
     }).compileComponents();
+
+    fixture =  TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UsersManagerSPA'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('UsersManagerSPA');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UsersManagerSPA app is running!');
+  it('should add the user', () => {
+    const addUserSpy = spyOn(
+      TestBed.get(UsersDataSource),
+      'addUser'
+    );
+    app.onAddUser({name: 'textUser'});
+    expect(addUserSpy).toHaveBeenCalled();
   });
 });
