002f3014337df463260dabfaa6dfc3b84c204849
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-11-20 14:01:52 +0000

    implement user form unit tests

--- src/app/components/user-form/user-form.component.spec.ts
+++ src/app/components/user-form/user-form.component.spec.ts
@@ -1,5 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { UserFormComponent } from './user-form.component';
 
 describe('UserFormComponent', () => {
@@ -8,7 +9,12 @@ describe('UserFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserFormComponent ]
+      imports: [
+        ReactiveFormsModule
+      ],
+      declarations: [
+        UserFormComponent
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +27,29 @@ describe('UserFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('creates a formGroup on init', () => {
+    component.ngOnInit();
+    expect(component.userForm).toBeDefined();
+  });
+
+  it('should emit userSubmited onSubmit', () => {
+    spyOn(component.userSubmited, 'emit');
+    component.ngOnInit();
+    component.userForm.get('name').setValue('testUserName');
+    expect(component.userForm.valid).toBeTruthy();
+
+    component.onSubmit();
+    expect(component.userSubmited.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit userSubmited onSubmit when user not valid', () => {
+    spyOn(component.userSubmited, 'emit');
+    component.ngOnInit();
+    expect(component.userForm.valid).toBeFalsy();
+
+    component.onSubmit();
+    expect(component.userSubmited.emit).not.toHaveBeenCalled();
   });
 });
